501944a242e988687d9cf1fa0d2ade21b1a96058
Luwen <example@gmail.com>
2025-12-05 16:26:33 +0800

    Initial Deploy v0.0.2 (404 & fblink & cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,10 +12,36 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const description =
+  "Asinta Architects is a design & build firm founded through the partnership of two young Filipino architects united by a shared vision — to deliver contemporary architectural solutions that are innovative, functional, and contextually relevant.";
+
 export const metadata: Metadata = {
   title: "Asinta Corp.",
-  description:
-    "Asinta Architects is a design & build firm founded through the partnership of two young Filipino architects united by a shared vision — to deliver contemporary architectural solutions that are innovative, functional, and contextually relevant.",
+  description,
+
+  openGraph: {
+    title: "Asinta Corp.",
+    description,
+    url: "https://asintacorp.vercel.app",
+    siteName: "Asinta Corp.",
+    images: [
+      {
+        url: "https://asintacorp.vercel.app/img/og_cover.webp",
+        width: 1200,
+        height: 630,
+        alt: "Asinta Corp. Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Asinta Corp.",
+    description,
+    images: ["https://asintacorp.vercel.app/img/og_cover.webp"],
+  },
 };
 
 export default function RootLayout({
